5bc420243f52c7fd0512861ee288f703be8d810c
Antoine Duchêne <antoine.duchene@imio.be>
2024-06-10 21:03:42 +0200

    WIP theming

--- src/plonetheme/deliberations/theme/index.js
+++ src/plonetheme/deliberations/theme/index.js
@@ -1,5 +1,30 @@
 import "./styles/theme.scss";
+import Watermark from '@uiw/watermark.js';
 
-if(module.hot) {
+const heightWidthRatio = 0.33;
+
+
+const applyWatermark = (el) => {
+  const watermark = document.createElement('div');
+  watermark.classList.add('watermark');
+  const wSize = (el.clientHeight + el.clientWidth)
+  watermark.style.height = wSize + "px";
+  watermark.style.width = wSize + "px";
+  watermark.style.left = ((-wSize / 2) + el.clientWidth / 2) + "px";
+  watermark.style.top = ((-wSize / 2) + el.clientHeight / 2) + "px";
+  const lengthMultiplier = Math.round(el.dataset.watermark.length);
+  watermark.dataset.text = (el.dataset.watermark + ' – ').repeat(Math.round(el.clientHeight * heightWidthRatio * lengthMultiplier));
+  el.appendChild(watermark);
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelectorAll('.watermarked').forEach(function (el) {
+    if (el.dataset.watermark) {
+      applyWatermark(el);
+    }
+  });
+});
+
+if (module.hot) {
   module.hot.accept();
 }
