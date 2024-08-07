00726ba1579dfa97729416ec52a46a0486ebd883
Antoine Duchêne <antoineduchene@icloud.com>
2024-08-07 20:39:42 +0200

    Faceted views + publication view

--- src/plonetheme/deliberations/theme/index.js
+++ src/plonetheme/deliberations/theme/index.js
@@ -1,43 +1,53 @@
 import "./styles/theme.scss";
-import Watermark from '@uiw/watermark.js';
+import "./styles/theme.scss";
+// import "bootstrap";
 
 const heightWidthRatio = 0.33;
 
 const createWatermark = (el, width, height, repeat) => {
-  const watermark = document.createElement('div');
-  watermark.classList.add('watermark');
-  const wSize = (height + width)
+  const watermark = document.createElement("div");
+  watermark.classList.add("watermark");
+  const wSize = height + width;
   watermark.style.height = wSize + "px";
   watermark.style.width = wSize + "px";
-  watermark.style.left = ((-wSize / 2) + width / 2) + "px";
-  watermark.style.top = ((-wSize / 2) + height / 2) + "px";
-  watermark.dataset.text = (el.dataset.watermark + ' – ').repeat(repeat);
+  watermark.style.left = -wSize / 2 + width / 2 + "px";
+  watermark.style.top = -wSize / 2 + height / 2 + "px";
+  watermark.dataset.text = (el.dataset.watermark + " – ").repeat(repeat);
   el.appendChild(watermark);
-}
+};
 
 const autoWatermark = (el) => {
   const lengthMultiplier = Math.round(el.dataset.watermark.length);
-  const repeat = Math.round(el.offsetHeight * heightWidthRatio * lengthMultiplier)
+  const repeat = Math.round(
+    el.offsetHeight * heightWidthRatio * lengthMultiplier
+  );
   if (el.dataset.watermark) {
     createWatermark(el, el.offsetWidth, el.offsetHeight, repeat);
   }
-}
+};
 
 const cleanWatermarks = () => {
-  document.querySelectorAll('.watermark').forEach(function (el) {
+  document.querySelectorAll(".watermark").forEach(function (el) {
     el.parentNode.removeChild(el);
   });
-}
+};
 
 const applyWatermarks = () => {
-  document.querySelectorAll('.watermarked').forEach(function (el) {
+  document.querySelectorAll(".watermarked").forEach(function (el) {
     autoWatermark(el);
   });
-}
+};
 
 document.addEventListener("DOMContentLoaded", function (event) {
-  applyWatermarks();
-  if (typeof Faceted != 'undefined') {
+  // const popoverTriggerList = document.querySelectorAll(
+  //   '[data-bs-toggle="popover"]'
+  // );
+  // console.log(popoverTriggerList);
+  // [...popoverTriggerList].map(
+  //   (popoverTriggerEl) => new Popover(popoverTriggerEl)
+  // );
+  // applyWatermarks();
+  if (typeof Faceted != "undefined") {
     jQuery(Faceted.Events).bind(Faceted.Events.AJAX_QUERY_SUCCESS, function () {
       setTimeout(() => {
         cleanWatermarks();
@@ -51,7 +61,6 @@ document.addEventListener("ItemsLayoutChanged", function (event) {
   cleanWatermarks();
   applyWatermarks();
 });
-
 
 // if (module.hot) {
 //   module.hot.accept();
